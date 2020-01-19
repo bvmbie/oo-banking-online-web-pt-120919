1b21c20cd80be91a05c86ddf4d234e1354d63185
Henna Vahatiitto <hnnamv@gmail.com>
2020-01-19 11:23:02 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
   
   def execute_transaction
     if @sender.balance < @amount && @status == "pending"
-      @sender.deposit( @amount =* -1 ) 
+      @sender.balance -= @amount 
       @receiver.deposit( @amount )
       @status = "complete"
     elsif @status = "complete"
