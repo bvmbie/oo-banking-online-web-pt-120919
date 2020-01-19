d536cd3b628e5aef0d52db53647e15350e33869e
Henna Vahatiitto <hnnamv@gmail.com>
2020-01-19 11:31:04 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -34,10 +34,10 @@ class Transfer
   end
   
   def reverse_transfer
-    if self.status == "complete"
-      self.sender.balance += amount
-      self.receiver.balance -= amount
-      self.status = "reversed"
+    if @status == "complete"
+      @sender.balance += amount
+      @receiver.balance -= amount
+      @status = "reversed"
     end
   end
   
