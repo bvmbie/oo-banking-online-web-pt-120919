818203610d05348021bc1199ba77206776cbad03
Henna Vahatiitto <hnnamv@gmail.com>
2020-01-19 11:01:46 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,8 +15,9 @@ class Transfer
   
   def execute_transaction
     if @sender.balance < @amount
-      @status = "rejected"
+      
       return "Transaction rejected. Please check your account balance."
+      @status = "rejected"
 
     elsif @status == "complete"
       puts "Transaction was already excuted"
