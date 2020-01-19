66efc829cfe22220f2597e967b44cddba914ac5a
Henna Vahatiitto <hnnamv@gmail.com>
2020-01-19 11:18:40 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
   
   def execute_transaction
-    if @sender.valid? == true && @status == "pending"
+    if @sender.bankvalid? == true && @status == "pending"
       @sender.deposit( @amount * -1 ) 
       @receiver.deposit( @amount )
       @status = "complete"
